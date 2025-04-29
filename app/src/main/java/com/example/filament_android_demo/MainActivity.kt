2c945262d1eb56a3abc489d92ab75b427f8a47fe
yuemenglong <yuemenglong@126.com>
2025-04-29 09:33:52 +0800

    按钮

--- app/src/main/java/com/example/filament_android_demo/MainActivity.kt
+++ app/src/main/java/com/example/filament_android_demo/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.filament_android_demo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.filament_android_demo.ui.theme.Filament_android_demoTheme
 
@@ -32,10 +36,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
+  val context = LocalContext.current
+  Column(modifier = modifier) {
+    Text(
+      text = "Hello $name!"
+    )
+    Button(onClick = {
+      Toast.makeText(context, "拍摄", Toast.LENGTH_SHORT).show()
+    }) {
+      Text("拍摄")
+    }
+  }
 }
 
 @Preview(showBackground = true)

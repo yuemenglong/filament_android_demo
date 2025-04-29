fbc43be39721f3420b8427ebf33013db5db0e2fd
yuemenglong <yuemenglong@126.com>
2025-04-29 09:26:10 +0800

    引入filament

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.filament:filament-android:1.57.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
